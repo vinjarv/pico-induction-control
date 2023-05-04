ee8b4b3fefea41b37483f100b77ae506170cf2ab
vinjarv <vinjar97@gmail.com>
2023-05-04 23:34:57 +0200

    Single shot ADC sampling

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,15 @@
 #include <pico.h>
 #include <hardware/adc.h>
 
-const int INDICATOR_PIN = LED_BUILTIN;
-
-uint16_t negative_treshold = (2^12 / 2) + 50; // Below this value current should be passing through the body diode of the IGBT
+uint16_t negative_treshold = (4096 / 2) + 50; // Below this value current should be passing through the body diode of the IGBT
                                               //TODO: Compute live based on AC voltage
-                                      
+                           
 // Code for calculating Mean Absolute Deviation of input signal for pan detection
 const uint16_t N_SAMPLES = 500;           // 1ms at 500ksps = 500 samples
 uint16_t input_buffer[N_SAMPLES];         // Buffer to store input samples
 uint32_t input_accumulator = 0;           // Accumulator for calculating mean
 uint16_t input_buffer_index = 0;          // Index of the input buffer
 bool pan_detection_on = false;            // Start bit
-
-bool cycle_negative = false;
-
-uint16_t avg_buffer[2];
-uint16_t moving_avg = 0;
-
-long last_time = 0;
-uint32_t shared_var = 0; // For performance monitoring: incremented once per ADC sample
 
 //
 // ---- Core 0 ----
@@ -53,25 +43,20 @@ void setup() {
 }
 
 void loop() {
+  static long last_time = millis();
   long current_time = millis();
   if (last_time + 1000 < current_time){
     last_time = current_time;
-    // Print how many times loop1() was executed in the last second
-    uint32_t temp = shared_var;
-    shared_var = 0;
-    Serial.print("Samples in the last second: ");
-    Serial.println(temp);
 
     if (!pan_detection_on) {
       // Get MAD and start next sampling
       computeMAD();
-      input_buffer_index = 0;
-      input_accumulator = 0;
-      pan_detection_on = true;
+      pan_detection_on = true; // Restart
+      for(int i = 0; i < N_SAMPLES; i++) {
+        Serial.println(input_buffer[i]); 
+      }
     }
   }
-    // Indicate if current voltage is below threshold
-    digitalWrite(INDICATOR_PIN, cycle_negative);
 }
 
 
@@ -82,7 +67,7 @@ void loop() {
 // ADC FIFO IRQ
 // Is triggered when the FIFO contains at least 1 sample
 //
-void my_adc_irq() {
+void __not_in_flash_func(adc_to_buffer)() {
   // Discard samples to get only most recent one
   while (adc_fifo_get_level() > 1) {
     adc_fifo_get(); // Discard sample
@@ -90,26 +75,16 @@ void my_adc_irq() {
   uint16_t sample = adc_fifo_get(); // Get most recent sample
 
   // Add sample to input buffer and accumulator if needed
-  if (pan_detection_on && input_buffer_index < N_SAMPLES) {
+  if (input_buffer_index < N_SAMPLES) {
     input_buffer[input_buffer_index] = sample;
     input_accumulator += sample;
     input_buffer_index++;
   }
-  // Check if we have enough samples
+  // Check if we have enough samples, then stop interrupt
   if (input_buffer_index == N_SAMPLES) {
-    pan_detection_on = false;
+    adc_run(false);
+    irq_clear(ADC_IRQ_FIFO);
   }
-
-  // Moving average
-  avg_buffer[1] = avg_buffer[0];
-  avg_buffer[0] = sample;
-  moving_avg = (avg_buffer[0] + avg_buffer[1]) / 2; // Division by powers of two should be fast, as it is just a bit shift
-
-  // Indicate if current voltage is below threshold
-  cycle_negative = moving_avg < negative_treshold;
-  
-  // Increment the shared variable to count how many times this IRQ was executed
-  shared_var++;
 }
 
 // Core 1 samples the analog input A0 as fast as possible
@@ -118,8 +93,14 @@ void setup1() {
   adc_init();
   adc_gpio_init(A0);                                    // Only init one pin
   adc_select_input(0);                                  // Select mux input
-  adc_set_clkdiv(0.0f);                                 // Set ADC clock to 48MHz / (96 + n) cycles per sample = 500kHz
+}
+
+void start_fast_adc() {
+  // Reset buffer
+  input_accumulator = 0;
+  input_buffer_index = 0;
   // Set up interrupt
+  adc_set_clkdiv(0.0f);                                 // Set ADC clock to 48MHz / (96 + n) cycles per sample = 500kHz
   adc_irq_set_enabled(true);
   adc_fifo_setup(
     true, // Write each completed conversion to the sample FIFO
@@ -128,12 +109,22 @@ void setup1() {
     false,// We won't see the ERR bit because of 1 sample threshold
     false // Don't shift each sample to 8 bits (useful when pushing to FIFO -> DMA to byte buffers)
   );
-  irq_set_exclusive_handler(ADC_IRQ_FIFO, my_adc_irq);  // Set ADC IRQ handler
+  irq_set_exclusive_handler(ADC_IRQ_FIFO, adc_to_buffer);  // Set ADC IRQ handler
   irq_set_enabled(ADC_IRQ_FIFO, true);                  // Enable IRQ on current core
   adc_run(true);                                        // Start ADC free running mode
 }
 
-// Core 1 is busy sampling the ADC, no need to do anything here
-// void loop1() {
-//   tight_loop_contents();
-// }
+// Core 1 main loop
+void loop1() {
+  // Wait for start signal, fill buffer with samples inside interrupt
+  static bool adc_running = false;
+  if (pan_detection_on && !adc_running){
+    start_fast_adc();
+    adc_running = true;
+  }
+  // Check for complete
+  if (adc_running && input_buffer_index == N_SAMPLES){
+    pan_detection_on = false;
+    adc_running = false;
+  }
+}
